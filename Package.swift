f33984d490e88473121d5d6fade7a21fa761bf71
phoenix <x.rhythm@qq.com>
2025-08-26 22:03:01 +0800

    add dependencies

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ReerJSONBenchmark",
+    platforms: [
+        .macOS(.v15),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/reers/ReerJSON.git", branch: "main"),
+        .package(url: "https://github.com/michaeleisel/ZippyJSON.git", from: "1.2.15"),
+        .package(url: "https://github.com/orlandos-nl/IkigaJSON.git", from: "2.3.2")
+    ],
+    targets: [
+        .executableTarget(
+            name: "ReerJSONBenchmark",
+            dependencies: [
+                .product(name: "ReerJSON", package: "ReerJSON" ),
+                .product(name: "ZippyJSON", package: "ZippyJSON"),
+                .product(name: "IkigaJSON", package: "IkigaJSON")
+            ],
+            resources: [
+                .copy("github_events.json")
+            ]
+        ),
+    ]
+)
